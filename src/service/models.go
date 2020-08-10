a040ac5be2d2ab15dbb9d88d7298fe579980e906
lxmhhy <1130010617@qq.com>
2020-08-10 19:57:11 +0800

    change models

--- src/service/models.go
+++ src/service/models.go
@@ -1,19 +1,29 @@
 package service
 
-type Host struct {
-	Ip       string   `json:"ip"`
-	Hostname string   `json:"hostname"`
-	MemSize  int64    `json:"mem_size"`  // kB
-	DiskSize int64    `json:"disk_size"` // KB
-	Class    string   `json:"class"`     // 物理机/虚拟机
-	Owner    []string `json:"owner"`     // 负责人，可能有多个
+import "Gin-IPs/src/configure"
+
+//|ID|主机名|IP|内存大小|磁盘大小|类型|负责人|
+
+type HostModel struct {
+	Oid      configure.Oid `json:"oid"` // 考虑所有实例存放在同一个集合中，需要一个字段来区分
+	Id       string        `json:"id"`
+	Ip       string        `json:"ip"`
+	Hostname string        `json:"hostname"`
+	MemSize  int64         `json:"mem_size"`
+	DiskSize int64         `json:"disk_size"`
+	Class    string        `json:"class"` // 主机类型
+	Owner    []string      `json:"owner"`
 }
 
-type Switch struct {
-	Name         string   `json:"name"` // 设备名
-	Ip           string   `json:"ip"`
-	Vip          []string `json:"vip"`          // 虚IP，可能有多个
-	ConsoleIp    string   `json:"console_ip"`   // 控制口IP
-	Manufacturer string   `json:"manufacturer"` // 厂家
-	Owner        []string `json:"owner"`        // 负责人数组
+//|ID|设备名|管理IP|虚IP|带外IP|厂家|负责人|
+
+type SwitchModel struct {
+	Oid           configure.Oid `json:"oid"` // 考虑所有实例存放在同一个集合中，需要一个字段来区分
+	Id            string        `json:"id"`
+	Name          string        `json:"name"`
+	Ip            string        `json:"ip"`
+	Vip           []string      `json:"vip"`
+	ConsoleIp     string        `json:"console_ip"`
+	Manufacturers string        `json:"manufacturers"` // 厂家
+	Owner         []string      `json:"owner"`
 }
